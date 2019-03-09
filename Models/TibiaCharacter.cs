e319b7091d75e0c7a2d644c8bcceb68631f613a7
kemaldev <kemaldev3@gmail.com>
2019-03-09 01:54:46 +0100

    Skeleton code

---
+++ Models/TibiaCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class TibiaCharacter
+    {
+        public string Name { get; set; }
+        public string GuildName { get; set; }
+        public string World { get; set; }
+        public string Vocation { get; set; }
+        public string Residence { get; set; }
+        public int Level { get; set; }
+        public bool IsOnline { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime LatestDeath { get; set; }
+        public List<string> UsuallyHunts { get; set; }
+    }
+}
